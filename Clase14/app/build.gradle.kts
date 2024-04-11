c23952a42ccdbc5079b2d9b8dd791431b8b7637e
Sinuhe Jaime <sierisimo@gmail.com>
2024-04-10 19:41:58 -0600

    Commit en vivo durante clase

---
+++ Clase14/app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit Jupiter for testing.
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    mainClass.set("com.codigofacilito.clase14.App")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ Clase14/app/src/main/java/com/codigofacilito/clase14/BasicTheory.java
@@ -0,0 +1,7 @@
+package com.codigofacilito.clase14;
+
+import java.io.*;
+
+public final class BasicTheory {
+
+}
